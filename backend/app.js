9c04926e9b45dfa61424362b8067a0fa3bf786b8
Prajwal100 <iar.prajwal@gmail.com>
2022-06-20 14:52:43 +0545

    latest update

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const errorMiddleware = require("./middlewares/errors");
+const path = require("path");
 
 app.use(cors());
 
@@ -14,6 +15,13 @@ app.use("/api/v1/user", user);
 app.use("/api/v1/celebrity", celebrity);
 app.use("/api/v1/movie", movie);
 
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
 //error middleware to handles
 app.use(errorMiddleware);
 
